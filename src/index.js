3477a9dcdb61b4d1c9cab4e6e3529d79c61b6461
AlekseiMinsk <budanov.info@gmail.com>
2019-02-24 15:43:26 +0300

    test complited 98

--- src/index.js
+++ src/index.js
@@ -1,34 +1,27 @@
 module.exports = function getZerosCount(number, base) {
-
-  function getAllFactorsFor(remainder) {
-    var factors = [], i;
-
-    for (i = 2; i <= remainder; i++) {
-        while ((remainder % i) === 0) {
-            factors.push(i);
-            remainder /= i;
-        }
+	function getAllFactorsFor(remainder) {
+		let factors = [], i;
+		for (i = 2; i <= remainder; i++) {
+		    while ((remainder % i) === 0) {
+		        factors.push(i);
+		        remainder /= i;
+		    }
+		}
+		return factors;
     }
-
-    return factors;
-}
-
-let arrFactors = getAllFactorsFor(base);
-let maxFactor = arrFactors[arrFactors.length - 1];
-let degreeOfMaxFactor = 0;
-arrFactors.forEach(item => {
-	if(item === maxFactor) {
-		degreeOfMaxFactor++;
-	} 
-})
-let maxFactorInDegree  = maxFactor;
-let quantity = 0;
-while((number / maxFactorInDegree) >= 1) {
-	quantity += Math.trunc(number / maxFactorInDegree);
-	maxFactorInDegree *= maxFactor;
-}
-return quantity/degreeOfMaxFactor;
-
-
-
+	let arrFactors = getAllFactorsFor(base);
+	let maxFactor = arrFactors[arrFactors.length - 1];
+	let degreeOfMaxFactor = 0;
+	arrFactors.forEach(item => {
+		if(item === maxFactor) {
+			degreeOfMaxFactor++;
+		} 
+	})
+	let maxFactorInDegree  = maxFactor;
+	let quantity = 0;
+	while((number / maxFactorInDegree) >= 1) {
+		quantity += Math.trunc(number / maxFactorInDegree);
+		maxFactorInDegree *= maxFactor;
+	}
+	return Math.trunc(quantity/degreeOfMaxFactor);
 }
