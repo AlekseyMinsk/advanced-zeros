a0c999bc76356b57f87024a1307b09d1cccad176
AlekseiMinsk <budanov.info@gmail.com>
2019-02-24 15:27:38 +0300

    tast complited

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+
+  function getAllFactorsFor(remainder) {
+    var factors = [], i;
+
+    for (i = 2; i <= remainder; i++) {
+        while ((remainder % i) === 0) {
+            factors.push(i);
+            remainder /= i;
+        }
+    }
+
+    return factors;
+}
+
+let arrFactors = getAllFactorsFor(base);
+let maxFactor = arrFactors[arrFactors.length - 1];
+let degreeOfMaxFactor = 0;
+arrFactors.forEach(item => {
+	if(item === maxFactor) {
+		degreeOfMaxFactor++;
+	} 
+})
+let maxFactorInDegree  = maxFactor;
+let quantity = 0;
+while((number / maxFactorInDegree) >= 1) {
+	quantity += Math.trunc(number / maxFactorInDegree);
+	maxFactorInDegree *= maxFactor;
+}
+return quantity/degreeOfMaxFactor;
+
+
+
 }
